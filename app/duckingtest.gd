212ce3fd44bd012ba53293134f8282bb5bb6468d
Anthony van Winkle <avanwinkle@users.noreply.github.com>
2022-12-28 13:29:01 -0800

    Support filtering and reading/writing data files

--- app/duckingtest.gd
+++ app/duckingtest.gd
@@ -6,14 +6,29 @@ var config = {
     "music_folder": null
 }
 
+var data = {
+    "__default__": { "track": "voice", "volume": 1.0, "ducking": {
+        "delay": 0.5,
+        "attack": 1.0,
+        "attenuation": 8.5,
+        "release_point": 0.5,
+        "release": 0.5
+    }}
+}
+
 func _ready():
     $Files/SourceFolder.pressed.connect(self.set_source_folder)
     $Files/MusicFolder.pressed.connect(self.set_music_folder)
     $Files/DataFile.pressed.connect(self.set_data_file)
-    $SoundList.item_selected.connect(self.enable_playback)
+    $SoundList.item_selected.connect(self.select_sound)
     $MusicList.item_selected.connect(self.play_music)
     $Play.disabled = true
+    $Save.disabled = true
+    $HideFinished.button_pressed = true
     $Play.pressed.connect(self.play_sound)
+    $Save.pressed.connect(self.save_sound)
+    $Write.pressed.connect(self.write_data_file)
+    $HideFinished.toggled.connect(self.toggle_hiding)
     
     self.load_config()
     
@@ -32,10 +47,12 @@ func load_config():
             if config_file.has_section_key("config", k):
                 config[k] = config_file.get_value("config", k)
     config.music_folder = "/Users/anthony/git/swords-of-vengeance/godot-mc/assets/music"
-    if config.get("source_folder"):    
-        self.set_source_folder(config.source_folder)
+    config.data_file = "/Users/anthony/git/swords-of-vengeance/godot-mc/autoloads/sounds.gd"
     if config.get("data_file"):
         self.set_data_file(config.data_file)
+        self.read_data_file()
+    if config.get("source_folder"):    
+        self.set_source_folder(config.source_folder)
     if config.get("music_folder"):
         self.set_music_folder(config.music_folder)
     print("Setup with config: %s" % config)
@@ -48,9 +65,19 @@ func save_config():
     if err != OK:
         print("Unable to save config file:", err)
 
-func enable_playback(index=-1):
+func select_sound(index=-1):
     $Play.disabled = false
-    $SoundList.item_selected.disconnect(self.enable_playback)
+    $Save.disabled = false
+    var filename = $SoundList.get_item_text(index)
+    print("Looking for filename %s in settings? %s" % [filename, data.has(filename)])
+    var settings = data.get(filename, data["__default__"])
+    $Settings/track.text = settings.track
+    $Settings/volume.text = str(settings.volume)
+    $Settings/delay.text = str(settings.ducking.delay)
+    $Settings/attack.text = str(settings.ducking.attack)
+    $Settings/attenuation.text = str(settings.ducking.attenuation)
+    $Settings/release_point.text = str(settings.ducking.release_point)
+    $Settings/release.text = str(settings.ducking.release)
 
 func play_music(name):
     var idx = $MusicList.get_selected_items()[0]
@@ -70,14 +97,24 @@ func play_sound():
     var path = $SoundList.get_item_metadata(idx)
     print("Playing sound at %s" % path)
   
-    
-    SoundPlayer.play("%s" % path, "voice", { "ducking": {
+    SoundPlayer.play("%s" % path, "voice", self.generate_sound_config(), true)
+
+func save_sound():
+    var idx = $SoundList.get_selected_items()[0]
+    var filename = $SoundList.get_item_text(idx)
+    data[filename] = self.generate_sound_config()
+
+func generate_sound_config():
+    return { 
+      "track": $Settings/track.text,
+      "volume": float($Settings/volume.text),
+      "ducking": {
         "delay": float($Settings/delay.text),
         "attack": float($Settings/attack.text),
         "attenuation": float($Settings/attenuation.text),
         "release_point": float($Settings/release_point.text),
         "release": float($Settings/release.text)
-    }}, true)
+    }}
 
 func prompt_for_folder():
     var dialog = FileDialog.new()
@@ -96,7 +133,11 @@ func set_source_folder(path=""):
     if path != "":
         self.parse_directory(path, $SoundList)
         $Files/SourceFolder.text = path
-        
+
+func toggle_hiding(_down):
+    $SoundList.clear()
+    self.parse_directory(config.source_folder, $SoundList)
+
 func set_music_folder(path=""):
     print("Set music folder")
     if path == "":
@@ -108,11 +149,17 @@ func set_music_folder(path=""):
         
 func set_data_file(path):
     print("Set data file")
-    config.data_file = path
-    self.save_config()
+    if path == "":
+        config["data_file"] = prompt_for_folder()
+    if path != "":
+      config.data_file = path
+      self.save_config()
+      $Files/DataFile.text = path
 
 func parse_directory(path: String, target: ItemList):
     var assets = []
+    var hide_existing = target == $SoundList and $HideFinished.button_pressed 
+    print("Parsing director with path %s, hide_existing is %s" % [path, hide_existing])
     var dir = DirAccess.open(path)
     if not dir:
       print("Unable to open path '%s'" % path)
@@ -126,11 +173,39 @@ func parse_directory(path: String, target: ItemList):
       else:
         var suffix = file_name.split(".")[-1]
         if suffix in ["wav", "ogg", "mp3"]:
-          print("Found file %s" % file_name)
-          assets.push_back({"name": file_name.split(".", 1)[0], "path": "%s/%s" % [path, file_name]})
+          #print("Found file %s" % file_name)
+          var short_name = file_name.split(".", 1)[0]
+          if hide_existing and data.has(short_name):
+            pass
+          else:
+            assets.push_back({"name": short_name, "path": "%s/%s" % [path, file_name]})
       file_name = dir.get_next()
     for asset in assets:
-      print("Adding asset '%s'" % asset.name)
+      #print("Adding asset '%s'" % asset.name)
       #b.pressed.connect(self.play_sound.bind(asset.path))
       var idx = target.add_item(asset.name)
       target.set_item_metadata(idx, asset.path)
+
+func read_data_file():
+    #var file = FileAccess.open(config.data_file, FileAccess.READ)
+    #var line = file.get_line()
+    #  while line:
+    #    if 
+    #    line = file.get_line()
+    var data_file = ResourceLoader.load(config.data_file)
+    print("Found a data file?")
+    print(data_file)
+    print(data_file.SoundFiles)
+    data = data_file.SoundFiles
+
+func write_data_file():
+    var file = FileAccess.open(config.data_file, FileAccess.WRITE)
+    for line in [
+        "# Auto-generated sound file config",
+        "extends Node",
+        "const SoundFiles = {",
+    ]:
+        file.store_line(line)
+    for k in data.keys():
+        file.store_line('    "%s": %s,' % [k, data[k]])
+    file.store_line("}")
